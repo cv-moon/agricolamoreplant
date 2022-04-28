bc5b98a56e19f0d1675ee274e14fe8302a6cbee5
cv-moon <cristian.chuquitarco92@gmail.com>
2022-04-28 16:24:03 -0500

    agregando rutas

---
+++ database/migrations/2022_04_28_154601_create_productos_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('categoria_id');
+            $table->unsignedBigInteger('unidad_id');
+            $table->unsignedBigInteger('tarifa_id');
+            $table->string('nombre', 300);
+            $table->string('cod_principal', 15)->unique();
+            $table->string('cod_auxiliar', 15)->unique()->nullable();
+            $table->string('composicion', 300)->nullable();
+            $table->decimal('pre_venta', 12, 3);
+            $table->decimal('por_descuento', 12, 3)->default(0);
+            $table->timestamps();
+
+            $table->foreign('categoria_id')->references('id')->on('categorias');
+            $table->foreign('unidad_id')->references('id')->on('unidades');
+            $table->foreign('tarifa_id')->references('id')->on('tarifas');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productos');
+    }
+}
