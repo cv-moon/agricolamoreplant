f8c738fb221bf1f119285ba13452f6ca6a7f6019
cv-moon <cristian.chuquitarco92@gmail.com>
2022-04-29 16:50:17 -0500

    cambio migracion de presentaciones

--- database/migrations/2022_04_28_205855_create_presentaciones_table.php
+++ database/migrations/2022_04_28_205855_create_presentaciones_table.php
@@ -15,15 +15,16 @@ class CreatePresentacionesTable extends Migration
     {
         Schema::create('presentaciones', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('categoria_id');
-            $table->unsignedBigInteger('tar_agregado_id');
-            $table->string('valor', 300)->unique();
-            $table->string('composicion', 300)->nullable();
+            $table->unsignedBigInteger('producto_id');
+            $table->unsignedBigInteger('unidades_id');
+            $table->string('cod_principal', 15)->unique();
+            $table->string('cod_auxiliar', 15)->unique()->nullable();
+            $table->tinyInteger('valor')->unique();
             $table->decimal('pre_venta', 15, 3)->default(0);
             $table->timestamps();
 
-            $table->foreign('categoria_id')->references('id')->on('categorias');
-            $table->foreign('tar_agregado_id')->references('id')->on('tar_agregados');
+            $table->foreign('producto_id')->references('id')->on('productos');
+            $table->foreign('unidades_id')->references('id')->on('unidades');
         });
     }
 
